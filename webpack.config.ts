a75e5cef5fa9ce9a4f81ac56cdc71913eb7bd156
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-04-09 10:37:08 +0300

    upd

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,26 @@
 // * - тк некоторые из библиотек не поддерживают default (import)
 import * as path from 'path';
 import * as webpack from 'webpack';
-import { BuildPaths } from "./config/build/types/config";
+import { BuildEnv, BuildPaths } from "./config/build/types/config";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
 
-const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'dist'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-}
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'dist'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+    
+    const mode = env.mode || "development";
+    const isDev = mode === "development";
+    const PORT = env.port || 3000;
+    
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths: paths,
+        isDev,
+        port: PORT,
+    });
 
-const mode = "development";
-const isDev = mode === "development";
-
-const config: webpack.Configuration = buildWebpackConfig({
-    mode,
-    paths: paths,
-    isDev
-});
-
-
-export default config;
+    return config;
+};
