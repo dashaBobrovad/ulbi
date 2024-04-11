ab6c819786794f72c7182f867788a4b324b62cee
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-04-11 22:30:41 +0300

    upd

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,19 @@ import * as webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
+    const stylesLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
+    
+    // babel уже включен в ts
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,6 +22,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     };
 
     return [
-        typescriptLoader,
+        typescriptLoader, stylesLoader
     ]
 } 
