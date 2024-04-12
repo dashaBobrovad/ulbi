ae23436a0afb05b8b77bf5c665be81fecfb93486
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-04-12 17:30:44 +0300

    upd

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,12 @@
 import * as webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
     const stylesLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
+            MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
             "css-loader",
             // Compiles Sass to CSS
