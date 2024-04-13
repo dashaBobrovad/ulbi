2e6252fcd14540c4ff3e41050419d22e22c1b959
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-04-13 21:04:18 +0300

    theme; custom hook; context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { Link, Route, Routes } from "react-router-dom";
 import AboutPageLazy from "./pages/AboutPage/AboutPage.lazy";
 import MainPageLazy from "./pages/MainPage/MainPage.lazy";
-import "./index.scss";
+import "../src/styles/index.scss";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <div>
+                <button onClick={toggleTheme}>TOGGLE THEME</button>
+            </div>
             <div>
                 <Link to="/">Главная</Link>
                 <Link to="/about">О нас</Link>{", "}
