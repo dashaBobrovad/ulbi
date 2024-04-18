d3c517e4f2221b7bf6059c86e56075fa54ee243b
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-04-18 22:19:21 +0300

    nav with theme

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,15 +1,35 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import { cls } from "shared/lib/cls";
+import { Link, LinkProps } from "react-router-dom";
 import cx from "./AppLink.module.scss";
 
-interface AppLinkProps {
-    className?: string;
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+};
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
 }
 
-export const AppLink: FC<AppLinkProps> = ({ className }) => {
+export const AppLink: FC<AppLinkProps> = (props) => {
+
+  const {
+    to,
+    className,
+    children,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+
   return (
-    <div className={cls(cx.applink, {}, [className])}>
-      
-    </div>
+    <Link
+      to={to}
+      className={cls(cx.applink, {}, [cx[theme], className])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
   )
 };
