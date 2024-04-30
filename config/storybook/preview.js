266a22baffd3715bdffce0fe88c764cbf07edddf
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-05-01 01:57:37 +0300

    sb color

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,11 +1,19 @@
-import { addDecorator } from '@storybook/react';
-import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-// import { ThemeDecorator } from './ThemeDecorator/ThemeDecorator';
-// import { Theme } from '../../src/app/providers/ThemeProvider';
-// import { RouterDecorator } from './RouterDecorator/RouterDecorator';
+import { addDecorator, configure } from "@storybook/react";
+import { Theme } from "app/providers/ThemeProvider";
+import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
+// import { ThemeDecorator } from "./ThemeDecorator/ThemeDecorator";
+// import { Theme } from "../../src/app/providers/ThemeProvider";
+// import { RouterDecorator } from "./RouterDecorator/RouterDecorator";
+
+function loadStories() {
+    document.body.className += `app ${Theme.LIGHT}`;
+    document.body.style.background = "none";
+}
+
+configure(loadStories, module);
 
 export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
         matchers: {
             color: /(background|color)$/i,
