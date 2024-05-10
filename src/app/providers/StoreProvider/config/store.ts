5c52b648cc3f51e8950a07e5612773d5a15e5e8a
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-05-10 15:51:18 +0300

    init redux toolkit

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { StateSChema } from "./StateSchema";
+
+export function createReduxStore(initialState?: StateSChema) {
+    return configureStore<StateSChema>({
+        reducer: { },
+        devTools: __IS_DEV__,
+    });
+}
+
+export const store = createReduxStore();
