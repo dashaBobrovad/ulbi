7c044cc8cccbebae1533430833e8f33b4d2bdb38
dashaBobrovad <dbobrova.dasha@gmail.com>
2024-05-13 22:20:14 +0300

    create User entity

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,9 +1,10 @@
 import { Counter } from "./ui/Counter";
 import type { CounterSchema } from "./modal/types/counterSchema";
-import { counterReducer } from "./modal/slice/counterSlice";
+import { counterReducer, counterActions } from "./modal/slice/counterSlice";
 
 export {
     Counter,
     CounterSchema,
     counterReducer,
+    counterActions,
 };
---
+++ src/entities/User/index.ts
@@ -0,0 +1,9 @@
+import type { User, UserSchema } from "./modal/types/userSchema";
+import { userReducer, userActions } from "./modal/slice/userSlice";
+
+export {
+    User,
+    UserSchema,
+    userReducer,
+    userActions,
+};
